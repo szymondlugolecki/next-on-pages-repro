96a6412850645a62835b503601dbaa3054bf7fee
Szymon Długołęcki <szymon.dlugolecki77@gmail.com>
2022-07-17 17:55:33 +0200

    updated next to @12.2.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export default function HomePage() {
+  const [data, setData] = useState([]);
+  async function getData() {
+    const res = await fetch('/api/user/elo');
+    const newData = await res.json();
+    setData(newData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <Welcome />
